bfe9662ba8b1e8769f415140f3abc8c354454979
Liz Lam <lizlam.217@gmail.com>
2018-01-25 23:29:17 -0800

    fix picture for scaling and add dropdown menu

--- js/scale.js
+++ js/scale.js
@@ -1,9 +1,9 @@
 $(function () {
     var $w = $(window).width();
     if ($w < 1400) {
-        $('.intro').css("background-image", "url(img/intro-667.jpg)");
+        $('.intro').css("background-image", "url(img/intro-667.png)");
     }
     else {
-        $('.intro').css("background-image", "url(img/intro-1334.jpg)");
+        $('.intro').css("background-image", "url(img/intro-1334.png)");
     }
 });
